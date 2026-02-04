915aa55ea7306b21181e25ea41f5d8d68a38a26e
Mario <mariomedpar@gmail.com>
2026-02-04 17:11:51 +0100

    Timer corregido

--- Software/laRVa_tp1_G1/timer.v
+++ Software/laRVa_tp1_G1/timer.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //////////////////////////////////////////////////////////////////
 //	TP1 - Sistemas electrónicos									//
 //	Grupo 1:												  	//
@@ -6,89 +5,51 @@
 //  Miguel Barrigón Gómez, Víctor Sánchez Valencia			    //
 //////////////////////////////////////////////////////////////////
 
+
+
+// Contador de periodos
 module TIMER( 
-    input clk, 
-    input rd, 
-    input wr, 
-    input [31:0] maximo, 
-    
-    output reg flag_temp,
-    output reg [31:0] cont 
+input clk, 
+input rd, 
+input wr, 
+input [31:0] maximo, 
+
+output reg flag_temp,
+output reg [31:0] cont
 ); 
 
 reg [31:0]aux=0; 
 
-// Valores iniciales al arranque
+// Ponemos los valores iniciales a 0
 initial 
-    begin  
-    cont = 0; 
-    flag_temp = 0; 
-    end 
+begin  
+cont = 0; 
+flag_temp = 0; 
+end 
 
 // Contador sincronizado con el reloj del sistema
 always @(posedge clk) 
-    begin 
-        // Escritura: carga nuevo periodo y reinicia contador/bandera
-        if (wr)  
-            begin 
-                aux <= maximo; 
-                cont <= 0; 
-                flag_temp <= 0; 
-            end 
-            
-        if(aux > 0) 
-            begin 
-                // Lectura: limpia la bandera para detectar nuevo evento
-                if(rd)  
-                    flag_temp <= 0;
-
-                // Compara con el periodo ajustado por el reloj (18)
-                if(cont == aux*18) 
-                    begin 
-                        cont <= 0; 
-                        flag_temp <= 1; 
-                    end 
-                else 
-                    cont <= (cont+1); 
-            end 
-    end 
-=======
-
-// TIMER MODULE
-module TIMER( 
-input clk, 
-input [31:0] maxcount, 
-input rd, 
-input wr, 
-output reg flagtimer, //Flag 
-output reg [31:0] contador 
-); 
-reg [31:0]aux=0; 
-initial 
-begin  
-contador = 0; 
-flagtimer = 0; 
-end 
-always @(posedge clk) 
 begin 
+// Escritura: carga nuevo periodo y reinicia contador/bandera
 if (wr)  
 begin 
-aux <= maxcount; 
-contador <= 0; 
-flagtimer <= 0; 
+aux <= maximo; 
+cont <= 0; 
+flag_temp <= 0; 
 end 
 if(aux > 0) 
 begin 
+// Lectura: limpia la bandera para detectar nuevo evento
 if(rd)  
-flagtimer <= 0; 
-if(contador == aux*18) 
+flag_temp <= 0; 
+// Compara con el periodo ajustado por el reloj (18)
+if(cont == aux*18) 
 begin 
-contador <= 0; 
-flagtimer <= 1; 
+cont <= 0; 
+flag_temp <= 1; 
 end 
 else 
-contador <= (contador+1); 
+cont <= (cont+1); 
 end 
 end 
->>>>>>> 4deec76f3dae665a120623fd9830555733c0b73a
 endmodule
